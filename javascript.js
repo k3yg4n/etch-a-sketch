37cdeea9c3eeb0cc59fdf2b66c1ba07b0d7cc97f
Keegan Liu <keeganliu5678@gmail.com>
2022-03-25 19:14:58 -0400

    Divs change coloured when hovered and clear button uncolours all divs.

--- javascript.js
+++ javascript.js
@@ -9,16 +9,34 @@ function makeSquareGrid(numRows){
 
     // Append a div to each cell of the grid
     for(let i = 1; i <= numRows*numRows; i++){
-        var cell = document.createElement("div");
+        let cell = document.createElement("div");
         cell.textContent = i;
-        //cell.classList.add("cell");
+        cell.classList.add("cell");
+        cell.addEventListener('mouseover',colorCell);
+
         gridContainer.appendChild(cell);
     }
 }
 
+function colorCell(e){
+    e.target.classList.add('coloredCell');
+}
+
+function clearGrid(e){
+    let cells = document.querySelectorAll(".cell");
+
+    cells.forEach(function(cell){
+        cell.classList.remove("coloredCell")
+    });
+}
+
 
 //// MAIN ////
+const clearButton = document.querySelector('#clearBtn');
 const gridContainer = document.querySelector('#grid');
+
+clearButton.addEventListener('click',clearGrid);
+
 makeSquareGrid(16);
 
 
